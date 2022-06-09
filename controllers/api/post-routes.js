4e8ed4793019134325926a9b05eb11d238b5e609
Megan Gedde <hoglu070@d.umn.edu>
2022-06-08 20:56:29 -0500

    added CRUD routes for posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment, Pet } = require('../../models');
 
-// get all users
+// get all posts
 router.get('/', (req, res) => {
     Post.findAll({
       attributes: ['id', 'last_seen_time', 'last_seen_street', 'last_seen_city', 'last_seen_state', 'last_seen_country', 'created_at'],
@@ -31,7 +31,8 @@ router.get('/', (req, res) => {
       });
   });
 
-  router.get('/:id', (req, res) => {
+  //get one post
+router.get('/:id', (req, res) => {
     Post.findOne({
       where: {
         id: req.params.id
@@ -39,17 +40,88 @@ router.get('/', (req, res) => {
       attributes: ['id', 'last_seen_time', 'last_seen_street', 'last_seen_city', 'last_seen_state', 'last_seen_country', 'created_at'],
       include: [
         {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-          model: User,
-          attributes: ['username']
-     }   },
-        {
-          model: User,
-          attributes: ['username']
+            model: Pet,
+            attributes: ['pet_name', 'species', 'breed', 'color']
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+            model: User,
+            attributes: ['username']
+       }   },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
         }
-      ]
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  //create a post
+router.post('/', withAuth, (req, res) => {
+    Post.create({
+        last_seen_time: req.body.last_seen_time,
+        last_seen_street: req.body.last_seen_street,
+        last_seen_city: req.body.last_seen_city,
+        last_seen_state: req.body.last_seen_state,
+        last_seen_country: req.body.last_seen_country,
+        //can change to req.session.user_id if using sessions
+        user_id: req.body.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//edit a post
+router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        last_seen_time: req.body.last_seen_time,
+        last_seen_street: req.body.last_seen_street,
+        last_seen_city: req.body.last_seen_city,
+        last_seen_state: req.body.last_seen_state,
+        last_seen_country: req.body.last_seen_country,
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  });
+
+  //delete a post
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
     })
       .then(dbPostData => {
         if (!dbPostData) {
@@ -63,3 +135,7 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+  
+  
+  
+    module.exports = router;
--- models/index.js
+++ models/index.js
@@ -43,4 +43,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Comment };
+module.exports = { User, Post, Comment, Pet };
