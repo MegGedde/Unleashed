d32f855e92485c4718822c5244c7b58f5615d773
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-06-15 07:11:02 -0500

    finished quick filters

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -89,41 +89,41 @@ router.get('/addpet', (req, res) => {
   });
 });
 
-//Get All in One Species
-// router.get('/post/:species', (req, res) => {
-//   Post.findAll({
-//     where: {
-//       species: req.params.species
-//     },
-//     attributes: ['id', 'last_seen_time', 'last_seen_street', 'last_seen_city', 'last_seen_state', 'last_seen_country', 'created_at'],
-//     include: [
-//       {
-//         model: Pet,
-//         attributes: ['pet_name', 'species', 'breed', 'color', 'when_encounter', 'photo'],
-//         include: {
-//           model: User,
-//           attributes: ['username']
-//         }
-//       },
-//       {
-//         model: User,
-//         attributes: ['username']
-//       }
-//     ]
-//   })
-//     .then(dbPostData => {
-//       // serialize data before passing to template
-//       const posts = dbPostData.map(post => post.get({ plain: true }));
-//       res.render('homepage', {
-//         posts,
-//         loggedIn: true
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+// Get All in One Species
+router.get('/filtered/:species', (req, res) => {
+  Post.findAll({
+    attributes: ['id', 'last_seen_time', 'last_seen_street', 'last_seen_city', 'last_seen_state', 'last_seen_country', 'created_at'],
+    include: [
+      {
+        model: Pet,
+        where: {
+          species: req.params.species
+        },
+        attributes: ['pet_name', 'species', 'breed', 'color', 'when_encounter', 'photo'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', {
+        posts,
+        loggedIn: true
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // DASHBOARD
 router.get('/dashboard', (req, res) => {
--- public/javascript/filtered.js
+++ public/javascript/filtered.js
@@ -1,75 +1,29 @@
-async function speciesDog() {
-  await fetch('/api/posts/Dog', {
-            method: 'GET',
-            body: JSON.stringify({
-                post_species
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+async function filterPosts(event) {
+    event.preventDefault();
 
-        if (response.ok) {
-            document.location.replace('/homepage/');
-        } else {
-            alert(response.statusText);
-        }
+    const species = event.target.getAttribute("id");
+
+    console.log(species);
+    
+    if(species){
+        document.location.replace(`/filtered/${species}`);
+
+    }
+
+    // const response = await fetch('/api/posts/', {
+    //     method: 'GET',
+    //     body: JSON.stringify({
+    //         species
+    //     }),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // });
+    // if (response.ok) {
+    //     document.location.replace('/homepage/');
+    // } else {
+    //     alert(response.statusText);
+    // }
 }
 
-async function speciesCat() {
-  await fetch('/post/Cat', {
-            method: 'GET',
-            body: JSON.stringify({
-                post_id
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            document.location.reload();
-        } else {
-            alert(response.statusText);
-        }
-}
-async function speciesReptile() {
-    await fetch('/post/Reptile', {
-              method: 'GET',
-              body: JSON.stringify({
-                  post_id
-              }),
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          });
-  
-          if (response.ok) {
-              document.location.reload();
-          } else {
-              alert(response.statusText);
-          }
-  }
-  async function speciesOther() {
-    await fetch('/post/Other', {
-              method: 'GET',
-              body: JSON.stringify({
-                  post_id
-              }),
-              headers: {
-                  'Content-Type': 'application/json'
-              }
-          });
-  
-          if (response.ok) {
-              document.location.reload();
-          } else {
-              alert(response.statusText);
-          }
-  }
-
-
-document.querySelector('.dogs').addEventListener('submit', speciesDog);
-document.querySelector('.cats').addEventListener('submit', speciesCat);
-document.querySelector('.reptile').addEventListener('submit', speciesReptile);
-document.querySelector('.other').addEventListener('submit', speciesOther);
+document.querySelector('.quick-search-form').addEventListener('click', filterPosts);
