2cc64496641cbb9aaa9f75e6c88f40a94947f5f8
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-06-16 08:42:04 -0500

    add pet form working with aws

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-// const multer  = require('multer')
-// const upload = multer({ dest: '../../uploads' })
+const multer  = require('multer')
+const upload = multer({ dest: '../../uploads' })
 const { Pet } = require('../../models')
-// const {uploadFile, downloadFile} = require('../../s3')
-// const fs = require('fs')
-// const util = require('fs')
-// const promisify = require('util.promisify')
+const {uploadFile, downloadFile} = require('../../s3')
+const fs = require('fs')
+const util = require('fs')
+const promisify = require('util.promisify')
 
-// const removeFile = promisify(fs.unlink)
+const removeFile = promisify(fs.unlink)
 
 // Get all pets 
 router.get('/all', (req, res) => {
@@ -115,58 +115,89 @@ router.put('/:id', (req, res) => {
 })
 
 // Downloads the image from AWS bucket and displays that image 
-// router.get('/images/:key', (req, res) => {
-//     const key = req.params.key
-//     const readStream = downloadFile(key)
+router.get('/images/:key', (req, res) => {
+    const key = req.params.key
+    const readStream = downloadFile(key)
 
     // Sends the image data right to the user
-    // readStream.pipe(res)
-    // res.redirect(`/images/${key}`)
-// })
+    readStream.pipe(res)
+    res.redirect(`/images/${key}`)
+})
  
 // Uses AWS Services
 // Grabs form data and creates a new Pet 
-// router.post('/create-pet', upload.single('photo'), async (req, res) => {
-//     const file = req.file
+router.post('/create-pet', upload.single('photo'), async (req, res) => {
+    const file = req.file
 
-//     // Uploads the image to AWS bucket 
-//     const result = await uploadFile(file)
+    // Uploads the image to AWS bucket 
+    const result = await uploadFile(file)
 
-//     // Grab form data
-//     const petName = req.body.pet_name
-//     const petAge = req.body.pet_age
-//     const petSpecies = req.body.species
-//     const petBreed = req.body.breed
-//     const petColor = req.body.color
-//     const petWhenEncounter = req.body.when_encounter
-//     const petPhoto = result.Key
-//     const petUniqueFeatures = req.body.unique_features
+    // Grab form data
+    const petName = req.body.pet_name
+    const petAge = req.body.pet_age
+    const petSpecies = req.body.species
+    const petBreed = req.body.breed
+    const petColor = req.body.color
+    const petWhenEncounter = req.body.when_encounter
+    const petPhoto = result.Key
+    const petUniqueFeatures = req.body.unique_features
 
-//     // Removes file from 'uploads' directory
-//     await removeFile(file.path)
-//     // res.redirect(`/api/pets/images/${result.Key}`)
-//     res.redirect('/')
+    // Removes file from 'uploads' directory
+    await removeFile(file.path)
+    // res.redirect(`/api/pets/images/${result.Key}`)
+    res.redirect('/')
 
-//     // Insert form data to create a new pet
-//     Pet.create({
-//         pet_name: petName,
-//         pet_age: petAge,
-//         species: petSpecies,
-//         breed: petBreed,
-//         color: petColor,
-//         when_encounter: petWhenEncounter,
-//         unique_features: petUniqueFeatures,
-//         photo: petPhoto,
-//         user_id: req.session.user_id
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   })
+    // Insert form data to create a new pet
+    Pet.create({
+        pet_name: petName,
+        pet_age: petAge,
+        species: petSpecies,
+        breed: petBreed,
+        color: petColor,
+        when_encounter: petWhenEncounter,
+        unique_features: petUniqueFeatures,
+        photo: petPhoto,
+        user_id: req.session.user_id
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  })
 
 
 
 module.exports = router
 
 
+{/* <form class="m-4" action="/api/pets/create-pet" method="post" enctype="multipart/form-data">
+<div class="form-header">
+   <h2>ADD A PET FORM</h2>
+</div>
+<h3>Pet Description:</h3>
+   <article class="m-4">
+       <label for="petName">*Name</label>
+       <input id="petName"  name="pet_name" type="text" placeholder="Pet Name">
+       <label for="petAge">Age</label>
+       <input id="petAge" name="pet_age" min="0" max="25" type="number" placeholder="Pet Age">
+       <select class="is-flex p-2 box my-2" name="species" id="">
+           <option value="" disabled selected>Pet Species</option>
+           <option value="dog">Dog</option>
+           <option value="cat">Cat</option>
+           <option value="reptile">Reptile</option>
+           <option value="other">Other</option>
+       </select>
+       <label for="petBreed">Breed</label>
+       <input id="petBrred" name="breed" type="text" placeholder="Breed">
+       <label for="petColor">Color</label>
+       <input id="petColor" name="color" type="text" placeholder="Color">
+       <label for="petUniqueFeatures">Does this pet have any unique features?</label>
+       <input id="petUniqueFeatures" name="unique_features" type="text" placeholder="...">
+       <label for="petWhenEncounter">What to do when encountered?</label>
+       <input id="petWhenEncounter" name="when_encounter" type="text" placeholder="...">
+       
+       <label for="uploadPhoto">Upload a photo</label>
+       <input id="uploadPhoto" name="photo" type="file" accept="image/png, image/jpg">
+       <button class="btn-generic m-0" type="submit">Create Pet</button>
+   </article>
+</form> */}
--- public/javascript/add-pet.js
+++ public/javascript/add-pet.js
@@ -1,45 +1,45 @@
-async function createPet(event) {
-    event.preventDefault();
+// async function createPet(event) {
+//     event.preventDefault();
 
-    const pet_name = document.querySelector('#pet-name').value;
-    const breed = document.querySelector('#breed').value;
-    const species = document.querySelector('#species').value;
-    const color = document.querySelector('#color').value;
-    const when_encounter = document.querySelector('#when-encounter').value;
-    const pet_age = document.querySelector('#age').value;
-    const unique_features = document.querySelector('#when-encounter').value;
+//     const pet_name = document.querySelector('#pet-name').value;
+//     const breed = document.querySelector('#breed').value;
+//     const species = document.querySelector('#species').value;
+//     const color = document.querySelector('#color').value;
+//     const when_encounter = document.querySelector('#when-encounter').value;
+//     const pet_age = document.querySelector('#age').value;
+//     const unique_features = document.querySelector('#when-encounter').value;
 
-    console.log(
-        species,
-        pet_name,
-        pet_age,
-        color,
-        breed,
-        unique_features,
-        when_encounter
-    );
+//     console.log(
+//         species,
+//         pet_name,
+//         pet_age,
+//         color,
+//         breed,
+//         unique_features,
+//         when_encounter
+//     );
 
-    const response = await fetch(`/api/pets/`, {
-        method: 'POST',
-        body: JSON.stringify({
-            pet_name,
-            species,
-            breed,
-            color,
-            when_encounter,
-            pet_age,
-            unique_features
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-            console.log(response.statusText);
-    }
-}
+//     const response = await fetch(`/api/pets/`, {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             pet_name,
+//             species,
+//             breed,
+//             color,
+//             when_encounter,
+//             pet_age,
+//             unique_features
+//         }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
+//     if (response.ok) {
+//         document.location.replace('/dashboard');
+//     } else {
+//         alert(response.statusText);
+//             console.log(response.statusText);
+//     }
+// }
 
-document.querySelector('#pet-info-form').addEventListener('submit', createPet);
+// document.querySelector('#pet-info-form').addEventListener('submit', createPet);
